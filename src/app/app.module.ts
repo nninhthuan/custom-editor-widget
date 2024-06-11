83ca31d4c7ef0aca88c57b269e3e90ec1829540e
Nguyen Ninh Thuan <nninhthuan211@gmail.com>
2024-06-11 13:20:53 +0700

    matching name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ChartModule } from '../../libs/ui/chart/chart.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ChartModule
+    BrowserModule
   ],
   providers: [
     provideClientHydration()
